e1c59eba51b97eb1436175a8741734db252f4365
Ricardo Perez <rxperez12@gmail.com>
2024-04-23 14:52:21 -0700

    serial class finished

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start=100):
+        """Create serial number generator with a start value"""
+        self.start = start
+        self.current_num = self.start
+
+    def generate(self):
+        """Returns next number in generator to user"""
+        self.current_num += 1
+        return self.current_num - 1
+
+    def reset(self):
+        """Resets self.current_num back to original start value"""
+        self.current_num = self.start
