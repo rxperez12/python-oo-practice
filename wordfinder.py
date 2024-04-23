20d12aa15264d8d871cdb4b77d31f4340e56d96e
Ricardo Perez <rxperez12@gmail.com>
2024-04-23 16:49:13 -0700

    finished wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,42 +1,55 @@
 import random
 
-PRACTICE_FILE_DIRECTORY = 'example.txt'
+PRACTICE_FILE_DIRECTORY = 'example2.txt'
 
 
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, file_path):
-        """Create model for word finder - Finds a random word in a given file
-        provided file has one word per line
+        """ Takes in file path and reads file into an array. Prints number of
+        words in file
         """
 
         self.file_path = file_path
-        self.file_data = []
-        self.word_counter = None
-        self.__read_file__()
-
-    def __read_file__(self):
-        """Read file adds it to self.file_data. Populates word_counter and prints
-        how many words were read"""
-        file_data = open(self.file_path, 'r')
-        counter = 0
-        for word in file_data:
-            self.file_data.append(word.replace('\n', ''))
-            counter += 1
-        self.word_counter = counter
+        self.file_words = self.__read_file__()
         self.__repr__()
 
+    def __read_file__(self):
+        """Reads file and returns array of words"""
+        file_data = open(self.file_path, 'r')
+        return [word.strip() for word in file_data]
+
     def __repr__(self):
-        print(f'{self.word_counter} words read')
+        print(f'{len(self.file_words)} words read')
 
     def random(self):
         """Returns random word from file_data"""
-        return random.choice(self.file_data)
+        return random.choice(self.file_words)
 
 
-wf = WordFinder(PRACTICE_FILE_DIRECTORY)
+class SpecialWordFinder(WordFinder):
+    """ Special Word Finder: finds random words from a file even including those
+    with invalid lines."""
 
+    # def __init__(self, file_path):
+    #     """ Takes in file path and reads file into an array. Prints out words
+    #     read"""
+
+    #     super().__init__(file_path)
+    #     self.file_words = self.__read_file__()
+
+    def __read_file__(self):
+        """Read file adds it to self.file_data. Disregards invalid lines"""
+        words_with_invalid = super().__read_file__()
+        return [word for word in words_with_invalid if word.isalpha()]
+
+
+wf = WordFinder('example2.txt')
 print(wf.random())
 print(wf.random())
-print(wf.random())
+
+
+swf = SpecialWordFinder('example2.txt')
+print(swf.random())
+print(swf.random())
